165e9c85813143da80e3504113805fc791ebd1ca
Spencer Transier <spencer@dayoneapp.com>
2019-11-16 21:46:52 -0800

    jUnit Initialization

---
+++ jUnit/src/MathCalculations.java
@@ -0,0 +1,12 @@
+public class MathCalculations {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public double calculateArea(double width, double height) {
+        return width * height;
+    }
+}
---
+++ jUnit/src/MathCalculationsTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+public class MathCalculationsTest {
+
+    public void
+
+/*
+    assertArrayEquals
+    assertEquals
+    assertFalse
+    assertNotNull
+    assertNotSame
+    assertNull
+    assertSame
+    assertThat
+    assertTrue
+
+ */
+}
