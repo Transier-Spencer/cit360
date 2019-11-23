154faa33a8980d97638bfcb049d17fec1fe20864
Spencer Transier <spencer@dayoneapp.com>
2019-11-22 22:30:53 -0800

    Updated Threading to use Executor

--- Threading/src/Main.java
+++ Threading/src/Main.java
@@ -1,7 +1,12 @@
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Create a new executor to run the threads
+        Executor executor = Executors.newCachedThreadPool();
 
         // Create new atomic variable
         AtomicInteger counter = new AtomicInteger(0);
@@ -13,8 +18,8 @@ public class Main {
         for (String recipe : recipes) {
             // Create a new thread for each recipe
             Thread thread = new Thread(new Recipe(recipe));
-            // Start each thread
-            thread.start();
+            // Use the executor to execute each thread
+            executor.execute(thread);
             // Increment atomic variable
             counter.incrementAndGet();
             // Print to console the atomic counter value
@@ -22,3 +27,5 @@ public class Main {
         }
     }
 }
+
+
